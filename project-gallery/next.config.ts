1d25d4cb4359f13fbc3356b4e929cbb15c453a0b
Sandip Neupane <sandipneupane775@gmail.com>
2025-01-25 22:13:34 +0545

    project initilization

---
+++ project-gallery/next.config.ts
@@ -0,0 +1,9 @@
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['www.pexels.com'], // Add the required domain here
+  },
+};
+
+export default nextConfig;
---
+++ project-gallery/src/app/page.tsx
@@ -0,0 +1,10 @@
+// import Image from "next/image";
+import ProjectGallery from "../components/projectgallery/project-gallery";
+
+export default function Home() {
+  return (
+    <>
+    <ProjectGallery />
+    </>
+  );
+}
